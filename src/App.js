645db9b2140e7a6b472692ef100c37ffeee901d9
PubuduArosha <it18108514@my.sliit.lk>
2020-04-26 01:43:44 +0530

    Update Image Bar

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <div>
         <Header/>
         <ImageBar/>
+        <br/>
         <Footer/>
     </div>
   );
--- src/components/image-bar/ImageBar.jsx
+++ src/components/image-bar/ImageBar.jsx
@@ -1,39 +1,53 @@
 import React from "react";
 import ImageBar3 from '../../images/ImageBar3.jpg'
+import ImageBar4 from '../../images/ImageBar4.jpg'
+import ImageBar5 from '../../images/ImageBar5.jpg'
 
 function ImageBar() {
     return(
         <div>
-            <div id="myCarousel" className="carousel slide" data-ride="carousel">
-
-                <ol className="carousel-indicators">
-                    <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
-                    <li data-target="#myCarousel" data-slide-to="1"></li>
-                    <li data-target="#myCarousel" data-slide-to="2"></li>
-                </ol>
-
-                <div className="carousel-inner">
-                    <div className="item active">
-                        <img src={ImageBar3} alt="ImageBar3" />
+            <div className="container">
+                <div id="myCarousel" className="carousel slide" data-ride="carousel">
+                    {/* Indicators */}
+                    <ol className="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to={0} className="active" />
+                        <li data-target="#myCarousel" data-slide-to={1} />
+                        <li data-target="#myCarousel" data-slide-to={2} />
+                    </ol>
+                    {/* Wrapper for slides */}
+                    <div className="carousel-inner">
+                        <div className="item active">
+                            <img src={ImageBar3} alt="Los Angeles" style={{width: '100%'}} />
+                            {/*<div className="carousel-caption">*/}
+                            {/*    <h3>Los Angeles</h3>*/}
+                            {/*    <p>LA is always so much fun!</p>*/}
+                            {/*</div>*/}
+                        </div>
+                        <div className="item">
+                            <img src={ImageBar4} alt="Chicago" style={{width: '100%'}} />
+                            {/*<div className="carousel-caption">*/}
+                            {/*    <h3>Chicago</h3>*/}
+                            {/*    <p>Thank you, Chicago!</p>*/}
+                            {/*</div>*/}
+                        </div>
+                        <div className="item">
+                            <img src={ImageBar5} alt="New York" style={{width: '100%'}} />
+                            {/*<div className="carousel-caption">*/}
+                            {/*    <h3>New York</h3>*/}
+                            {/*    <p>We love the Big Apple!</p>*/}
+                            {/*</div>*/}
+                        </div>
                     </div>
-
-                    <div className="item">
-                        <img src={ImageBar3} alt="ImageBar3" />
-                    </div>
-
-                    <div className="item">
-                        <img src={ImageBar3} alt="ImageBar3" />
-                    </div>
+                    {/* Left and right controls */}
+                    <a className="left carousel-control" href="#myCarousel" data-slide="prev">
+                        <span className="glyphicon glyphicon-chevron-left" />
+                        <span className="sr-only">Previous</span>
+                    </a>
+                    <a className="right carousel-control" href="#myCarousel" data-slide="next">
+                        <span className="glyphicon glyphicon-chevron-right" />
+                        <span className="sr-only">Next</span>
+                    </a>
                 </div>
-
-                <a className="left carousel-control" href="#myCarousel" data-slide="prev">
-                    <span className="glyphicon glyphicon-chevron-left"></span>
-                    <span className="sr-only">Previous</span>
-                </a>
-                <a className="right carousel-control" href="#myCarousel" data-slide="next">
-                    <span className="glyphicon glyphicon-chevron-right"></span>
-                    <span className="sr-only">Next</span>
-                </a>
             </div>
         </div>
     );
