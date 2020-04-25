5eebd230b53b1e042df4d6a55ea3702fb2465ab4
PubuduArosha <it18108514@my.sliit.lk>
2020-04-26 00:31:17 +0530

    Add Header.jsx

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Header() {
+    return(
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <a className="navbar-brand" href="/home">Chauffeurprive Sri Lanka</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
+                        <a className="nav-item nav-link active" href="#">Destinations <span className="sr-only">(current)</span></a>
+                        <a className="nav-item nav-link active" href="#">Tours <span className="sr-only">(current)</span></a>
+                        <a className="nav-item nav-link active" href="#">Activities <span className="sr-only">(current)</span></a>
+                        <a className="nav-item nav-link active" href="#">Contact <span className="sr-only">(current)</span></a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
