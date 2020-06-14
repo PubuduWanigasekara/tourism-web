847459895fcb771ba4fb9e7657d7d14cfd99105d
PubuduArosha <it18108514@my.sliit.lk>
2020-06-14 12:28:04 +0530

    Nav update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,28 +1,57 @@
 import React from "react";
+import './Header.css';
 
 function Header() {
-    return(
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light shadow">
-                <a className="navbar-brand" href="/home"><b>Chauffeur Prive Sri Lanka</b></a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <a className="nav-item nav-link active" href="#"><b>Home </b><span className="sr-only">(current)</span></a>
-                        <a className="nav-item nav-link active" href="#"><b>Destinations </b><span className="sr-only">(current)</span></a>
-                        <a className="nav-item nav-link active" href="#"><b>Tours </b><span className="sr-only">(current)</span></a>
-                        <a className="nav-item nav-link active" href="#"><b>Activities </b><span className="sr-only">(current)</span></a>
-                        <a className="nav-item nav-link active" href="#"><b>About </b><span className="sr-only">(current)</span></a>
-                        <a className="nav-item nav-link active" href="#"><b>Contact </b><span className="sr-only">(current)</span></a>
-                    </div>
-                </div>
-            </nav>
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light shadow fixed-top ">
+        <a className="navbar-brand" href="/home">
+          <b>Chauffeur Prive Sri Lanka</b>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="navbar-collapse collapse w-100 order-3 dual-collapse2"id="navbarNavAltMarkup"
+        >
+          <div className="navbar-nav ">
+            <a className="nav-item nav-link active" href="#">
+              <b>Home </b>
+              <span className="sr-only">(current)</span>
+            </a>
+            <a className="nav-item nav-link active" href="#">
+              <b>Destinations </b>
+              <span className="sr-only">(current)</span>
+            </a>
+            <a className="nav-item nav-link active" href="#">
+              <b>Tours </b>
+              <span className="sr-only">(current)</span>
+            </a>
+            <a className="nav-item nav-link active" href="#">
+              <b>Activities </b>
+              <span className="sr-only">(current)</span>
+            </a>
+            <a className="nav-item nav-link active" href="#">
+              <b>About </b>
+              <span className="sr-only">(current)</span>
+            </a>
+            <a className="nav-item nav-link active" href="#">
+              <b>Contact </b>
+              <span className="sr-only">(current)</span>
+            </a>
+          </div>
         </div>
-    );
+      </nav>
+    </div>
+  );
 }
 
 export default Header;
