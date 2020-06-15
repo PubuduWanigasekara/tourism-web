bb4c291f001227b79674c8b82c26658c0951ce20
PubuduArosha <it18108514@my.sliit.lk>
2020-06-16 01:04:00 +0530

    Update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,22 +4,20 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="opfooter">
-      <div class="card text-left">
-        <div class="card-body">
-          <h3 class="card-title">
+      <div className="card text-left">
+        <div className="card-body">
+          <h4 className="card-title">
             <b>Chauffeur Prive Sri Lanka</b>
-          </h3>
-          <p class="card-text">
+          </h4>
+          <p className="card-text">
             Chauffeurprive Srilanka is a specialist in tour oraganization
-            focused on offering our clients luxury holiday <br/>packages and the
-            opportunity to enjoy Srilanka's beauty making your experience with
-            us a momentous occasion. <br />
-            <b>+94 75 9485 435</b> <br />
+            focused on offering our clients luxury holiday packages <br />
+            and the opportunity to enjoy Srilanka's beauty making your
+            experience with us a momentous occasion. <br />
             <b>srilankachauffeurprive@gmail.com</b>
+            <b> | +94 75 9485 435</b>
           </p>
-          {/* <a href="#" class="btn btn-primary">
-            Go somewhere
-          </a> */}
+          
         </div>
         <div class="card-footer text-muted text-center">
           <b>Powered by OPENMIC Digital Media</b>
