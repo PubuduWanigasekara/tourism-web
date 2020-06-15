810690cc8dd4cc8f490ec1cf1af97cc4703cedc6
PubuduArosha <it18108514@my.sliit.lk>
2020-06-15 23:45:02 +0530

    Footer update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,11 +4,20 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="opfooter">
-      <nav className="navbar fixed-bottom navbar-dark bg-dark">
-        <a className="navbar-brand" href="#">
-          Powered by OPENMIC Digital Media
-        </a>
-      </nav>
+      <div class="card text-center">
+        <div class="card-header">Featured</div>
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+          <a href="#" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+        <div class="card-footer text-muted">2 days ago</div>
+      </div>
     </div>
   );
 }
