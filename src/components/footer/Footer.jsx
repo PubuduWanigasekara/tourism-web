3b634338cae2f9e42b061bb25db24336261846f3
PubuduArosha <it18108514@my.sliit.lk>
2020-06-15 23:59:17 +0530

    Footer update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,8 +9,12 @@ function Footer() {
         <div class="card-body">
           <h5 class="card-title">Special title treatment</h5>
           <p class="card-text">
-            With supporting text below as a natural lead-in to additional
-            content.
+            <b>Chauffeur Prive Sri Lanka</b>
+            Chauffeurprive Srilanka is a specialist in tour oraganization
+            focused on offering our clients luxury holiday packages and the
+            opportunity to enjoy Srilanka's beauty making your experience with
+            us a momentous occasion. +94 75 9485 435
+            srilankachauffeurprive@gmail.com
           </p>
           <a href="#" class="btn btn-primary">
             Go somewhere
