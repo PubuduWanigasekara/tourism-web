3e221566766a1b6531358d2c3a785a6c66b5dfad
PubuduArosha <it18108514@my.sliit.lk>
2020-06-21 22:32:06 +0530

    Destination cards update

--- src/components/destinations/Destinations.jsx
+++ src/components/destinations/Destinations.jsx
@@ -1,76 +1,55 @@
 import React from "react";
+import "./Destinations.css";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
+import Capture from "../../images/Capture.jpg";
 
 function Destinations() {
   return (
     <div>
       <Header />
       <div className="container5">
-        <div className="card w-150">
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Button
-            </a>
+        <div>
+          {/* Header */}
+          <div className="header">
+            <h1>Responsive Image Grid</h1>
+            <p>Resize the browser window to see the responsive effect.</p>
           </div>
-        </div>
-
-        <div className="card" style={{ width: "500px" }}>
-          <img
-            className="card-img-top"
-            src="../../images/IMG_20180317_154944.jpg"
-            alt="Card image"
-          />
-          <div className="card-img-overlay">
-            <h4 className="card-title">John Doe</h4>
-            <p className="card-text">Some example text.</p>
-            <a href="#" className="btn btn-primary">
-              See Profile
-            </a>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="card w-50">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Button
-              </a>
+          {/* Photo Grid */}
+          <div className="row">
+            <div className="column">
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
             </div>
-          </div>
-          <div className="card w-25">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Button
-              </a>
+            <div className="column">
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
             </div>
-          </div>
-
-          <div className="card w-25">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Button
-              </a>
+            <div className="column">
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
+            </div>
+            <div className="column">
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
+              <img src={Capture} style={{ width: "100%" }} />
             </div>
           </div>
         </div>
