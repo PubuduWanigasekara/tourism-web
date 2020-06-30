34bd2e948e59a5ae90eec694512ac9208456d799
PubuduArosha <it18108514@my.sliit.lk>
2020-06-30 21:59:34 +0530

    Add new routes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,30 @@ import Home from "./components/home/Home";
 import Tours from "./components/tours/Tours.jsx";
 import Ampara from "./components/destinations/details/Ampara.jsx";
 import Anuradhapura from "./components/destinations/details/Anuradhapura.jsx";
+import Bandarawela from "./components/destinations/details/Bandarawela.jsx";
+import Batticaloa from "./components/destinations/details/Batticaloa.jsx";
+import Bentota from "./components/destinations/details/Bentota.jsx";
+import Beruwala from "./components/destinations/details/Beruwala.jsx";
+import Colombo from "./components/destinations/details/Colombo.jsx";
+import Galle from "./components/destinations/details/Galle.jsx";
+import Haputale from "./components/destinations/details/Haputale.jsx";
+import Hikkaduwa from "./components/destinations/details/Hikkaduwa.jsx";
+import Kandy from "./components/destinations/details/Kandy.jsx";
+import Kitulgala from "./components/destinations/details/Kitulgala.jsx";
+import Kurunagala from "./components/destinations/details/Kurunagala.jsx";
+import MannarIsland from "./components/destinations/details/MannarIsland.jsx";
+import Matale from "./components/destinations/details/Matale.jsx";
+import Mihinthale from "./components/destinations/details/Mihinthale.jsx";
+import Mirissa from "./components/destinations/details/Mirissa.jsx";
+import Monaragala from "./components/destinations/details/Monaragala.jsx";
+import Negambo from "./components/destinations/details/Negambo.jsx";
+import NuwaraEliya from "./components/destinations/details/NuwaraEliya.jsx";
+import Polonnaruwa from "./components/destinations/details/Polonnaruwa.jsx";
+import Tangalle from "./components/destinations/details/Tangalle.jsx";
+import TheSacredMountain from "./components/destinations/details/TheSacredMountain.jsx";
+import Tissamaharama from "./components/destinations/details/Tissamaharama.jsx";
+import Trincomalee from "./components/destinations/details/Trincomalee.jsx";
+import VavuniyaAndJaffna from "./components/destinations/details/VavuniyaAndJaffna.jsx";
 
 import Activities from "./components/activities/Activities.jsx";
 
@@ -43,6 +67,81 @@ function App() {
             </Route>
             <Route path="/destinations-anuradhapura">
               <Anuradhapura />
+            </Route>
+            <Route path="/destinations-anuradhapura">
+              <Anuradhapura />
+            </Route>
+            <Route path="/destinations-bandarawela">
+              <Bandarawela />
+            </Route>
+            <Route path="/destinations-batticaloa">
+              <Batticaloa />
+            </Route>
+            <Route path="/destinations-bentota">
+              <Bentota />
+            </Route>
+            <Route path="/destinations-beruwala">
+              <Beruwala />
+            </Route>
+            <Route path="/destinations-colombo">
+              <Colombo />
+            </Route>
+            <Route path="/destinations-galle">
+              <Galle />
+            </Route>
+            <Route path="/destinations-haputale">
+              <Haputale />
+            </Route>
+            <Route path="/destinations-hikkaduwa">
+              <Hikkaduwa />
+            </Route>
+            <Route path="/destinations-kandy">
+              <Kandy />
+            </Route>
+            <Route path="/destinations-kitulgala">
+              <Kitulgala />
+            </Route>
+            <Route path="/destinations-kurunagala">
+              <Kurunagala />
+            </Route>
+            <Route path="/destinations-mannarisland">
+              <MannarIsland />
+            </Route>
+            <Route path="/destinations-matale">
+              <Matale />
+            </Route>
+            <Route path="/destinations-mihinthale">
+              <Mihinthale />
+            </Route>
+            <Route path="/destinations-mirissa">
+              <Mirissa />
+            </Route>
+            <Route path="/destinations-monaragala">
+              <Monaragala />
+            </Route>
+            <Route path="/destinations-negambo">
+              <Negambo />
+            </Route>
+            <Route path="/destinations-nuwaraeliya">
+              <NuwaraEliya />
+            </Route>
+            <Route path="/destinations-polonnaruwa">
+              <Polonnaruwa />
+            </Route>
+            <Route path="/destinations-tangalle">
+              <Tangalle />
+            </Route>
+            <Route path="/destinations-thesacredmountain">
+              <TheSacredMountain />
+            </Route>
+            <Route path="/destinations-tissamaharama">
+              <Tissamaharama />
+            </Route>
+            <Route path="/destinations-trincomalee">
+              <Trincomalee />
+            </Route>
+            <Route path="/destinations-vavuniyaandjaffna">
+              <VavuniyaAndJaffna />
             </Route>
             <Route path="/tours">
               <Tours />
