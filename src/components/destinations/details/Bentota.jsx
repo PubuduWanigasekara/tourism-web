136842576d5d2fa6d9f6064c19cfcf2f82cdc4ba
PubuduArosha <it18108514@my.sliit.lk>
2020-07-01 21:52:45 +0530

    Add Destinations Details

--- src/components/destinations/details/Bentota.jsx
+++ src/components/destinations/details/Bentota.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import AmparaI from "../../../images/destinations/details/ampara.jpg";
+import Image from "../../../images/destinations/details/ampara.jpg";
 
 function Bentota() {
   return (
@@ -8,7 +8,7 @@ function Bentota() {
       <div className="card mb-3">
         <h5 className="card-title">
           <h1>
-            <b>Ampara</b>
+            <b>Bentota</b>
           </h1>
           <Link to="/destinations">
             <button className="btn btn-secondary">Back</button>
@@ -16,84 +16,88 @@ function Bentota() {
         </h5>
         <img
           className="card-img-top"
-          src={AmparaI}
+          src={Image}
           alt="Card image cap"
           style={{}}
         />
         <div className="card-body">
           <p className="card-text">
             <h3>
-              This was a hunter’s resting place during British colonial days
-              (Late 1890s and early 1900). During the development of the Galoya
-              scheme from 1949, this was transformed into a town.
+              Bentota is well experienced with what tourists want so you can
+              arrange almost anything from swimming, diving and fishing to water
+              sports like water skiing, wind – surfing, jet skiing and tubing.
+              Bentota enriched swimming conditions in the season combined with
+              fun water sport and exiting excursions that setup an ideal
+              background for the relax holiday in Sri Lanka is an attraction for
+              the families . The accommodation option at Bentota ranges from 5
+              star luxury to mid-range and budget.
               {/* <br/><br/>
               <b>Top attractions in Ampara : </b> */}
             </h3>
 
             <h3 className="card-title">
-              <b>Buddhangala Monastery</b>
+              <b>Bentota Beach</b>
               <h4>
-                Hidden among a thick jungle full of wild elephants and leopards
-                is the Buddhangala monastery, a Buddhist meditation center,
-                which is nearby 2300 years old. Established in the secondary
-                century BC by a provincial princess named, Chitra.
+                The beach Bentota is nothing like your usual holiday destination
+                and the beach here is a fold of pristine golden sands along the
+                mighty Indian Ocean. Bentota’s principal dive is Canoe Rock
+                where you will find sandy areas as well as sections with coral
+                formations. Divers have the shore, with the former being more
+                popular and perhaps more exiting a night dive is a good option
+                for the enthusiast who caves adventure. Crucially, the Majority
+                of diving operators take supplies of pure oxygen with them to
+                counter the risk of decompression.
               </h4>
             </h3>
 
             <h3 className="card-title">
-              <b>Jayanthi Wewa (lake) </b>
+              <b>Picturesque Location </b>
               <h4>
-                The reservoir is a source of water to the elephant herds who
-                roam the bordering gal oya national wildlife park. And the
-                flocks of bird like little cormorant, Malayan night heron,
-                lesser whistling – duck and spot billed pelican, who gather at
-                the reservoir in search of food and nesting ground.
+                Bentota is the most popular location for romantic weddings, is
+                witnesses hundreds of weddings of locals as well as the
+                foreigners. Is also an attraction of honeymoon couples once
+                again to Sri Lankans as well as to the foreigners. At the
+                northern end of the beach, is the long and narrow estuary called
+                “Paradise Island”, at the southern end is the pristine palm
+                fringed broad beach.
               </h4>
             </h3>
 
             <h3 className="card-title">
-              <b>Wadinagala </b>
+              <b>Turtle Hatchery </b>
               <h4>
-                Stands 2400 feet tall challenging the brave and the reckless. It
-                is famed to be a mountain, one considered as an alternative to
-                bear the foot marks of the Lord Buddha and its heritage in
-                ancient Buddhism is visible in many ruins scattered around the
-                mountain. Beautiful guard stone s, rocks pillars and moonstones
-                lie amidst the towering forest of ebony and stain wood, which is
-                been plundered by treasure hunters and illegal loggers.
+                Kosgoda nearby Bentota is famous for its turtle hatchery
+                operated by the wild life protection society of Sri Lanka. It
+                was established 1981 to protect Sri Lanka’s turtles from
+                extinction. The hatchery pays fishermen for eggs that they
+                collect at night along the sandy beach; visitors can see huge
+                tanks filled with new born turtle hatchlings. <br />
+                <br />
+                After the baby turtles are being fed they are taken to be
+                released when they are 2-4 days old, usually during the safer
+                hours of darkness. Although October to April is the main laying
+                season and some eggs can be found at Wasgoda throughout the
+                year. Marine turtles were roaming the oceans for about 190
+                million years. Among the many different varieties of this
+                species only eight of ancient reptiles are found living today
+                Sri Lankan beaches. The Turtle hatcheries are open from 6.00 am
+                to 6.00pm and in the evening if you visit here, you can get a
+                chance to release a freshly hatched three day old turtle into
+                the ocean.
               </h4>
             </h3>
 
             <h3 className="card-title">
-              <b>Galoya National Park</b>
+              <b>Brief Garden</b>
               <h4>
-                Established in 1954 and serves as the main catchment area for
-                Senanayake Samudra, the largest reservoir in Sri Lanka.
-              </h4>
-            </h3>
-
-            <h3 className="card-title">
-              <b>Crocodile Rock </b>
-              <h4>
-                Lies at the river mouth where the Heda Oya falls to sea. Some
-                drip ledge caves, rock cut steps and ruins of ancient stupa can
-                be found on this rock.
-              </h4>
-            </h3>
-            <h3 className="card-title">
-              <b>Lahugala Magul Maha Viharaya </b>
-              <h4>
-                Ruins of this temple are one of the major tourist attractions of
-                the eastern province. And this temple is an archeologically
-                protected monument of the country.
-              </h4>
-            </h3>
-
-            <h3 className="card-title">
-              <b>Lahugala Kitulana National Park </b>
-              <h4>
-                One of the smallest national parks in Sri Lanka and it is an
-                important habitat for Sri Lankan elephants and endemic birds.
+                Brief garden is a wonderful stretch of impeccably designed
+                landscape, spread over a couple of acres of land, which is,
+                located about eight kilometers off Bentota. Brief garden house
+                is a mesmerizingly beautiful Japanese style gardens and lawns
+                inspired from the colonial days. It has been home to the famous
+                architect Bevis Bawa, some of whose works are showcased here,
+                along with several other artists from Sri Lanka as well as
+                abroad.
               </h4>
             </h3>
           </p>
--- src/components/destinations/details/Beruwala.jsx
+++ src/components/destinations/details/Beruwala.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import AmparaI from "../../../images/destinations/details/ampara.jpg";
+import Image from "../../../images/destinations/details/ampara.jpg";
 
 function Beruwala() {
   return (
@@ -8,7 +8,7 @@ function Beruwala() {
       <div className="card mb-3">
         <h5 className="card-title">
           <h1>
-            <b>Ampara</b>
+            <b>Beruwala</b>
           </h1>
           <Link to="/destinations">
             <button className="btn btn-secondary">Back</button>
@@ -16,84 +16,58 @@ function Beruwala() {
         </h5>
         <img
           className="card-img-top"
-          src={AmparaI}
+          src={Image}
           alt="Card image cap"
           style={{}}
         />
         <div className="card-body">
           <p className="card-text">
             <h3>
-              This was a hunter’s resting place during British colonial days
-              (Late 1890s and early 1900). During the development of the Galoya
-              scheme from 1949, this was transformed into a town.
+              Powdery sands, towering palm and heavenly coral reefed bay –
+              Beruwala is nothing short of beach holiday bliss. ’ If Sri Lanka
+              has one all out beach resort, Beruwala it’s a plum spot for
+              serious beach lovers thanks to its dazzling combination of sun,
+              sea and sand. The name Beruwala is derived from the Sinhalese word
+              for the place where the sail is lowered. It marks the spot for the
+              first Muslim settlement on the island, established by Arabian
+              traders around 8th century AD.
               {/* <br/><br/>
               <b>Top attractions in Ampara : </b> */}
             </h3>
 
             <h3 className="card-title">
-              <b>Buddhangala Monastery</b>
+              <b>Kechimalai Mosque</b>
               <h4>
-                Hidden among a thick jungle full of wild elephants and leopards
-                is the Buddhangala monastery, a Buddhist meditation center,
-                which is nearby 2300 years old. Established in the secondary
-                century BC by a provincial princess named, Chitra.
+                Beruwala is home to the oldest mosque in the island, the
+                kechimalai mosque is built on the spot where the Muslim traders
+                from the Middle East landed. The best time to visit this
+                historic structure would be during the annual celebration
+                marking the end of the month of Ramadan .it is a joyous occasion
+                and visitors to the kechimalai mosque will also enjoy the
+                gorgeous vistas of the ocean and the surrounding areas that one
+                can get from a top the headland.
               </h4>
             </h3>
 
             <h3 className="card-title">
-              <b>Jayanthi Wewa (lake) </b>
+              <b>Beruwala Light House</b>
               <h4>
-                The reservoir is a source of water to the elephant herds who
-                roam the bordering gal oya national wildlife park. And the
-                flocks of bird like little cormorant, Malayan night heron,
-                lesser whistling – duck and spot billed pelican, who gather at
-                the reservoir in search of food and nesting ground.
+                A ten minute boat ride to this incredible island will take you
+                to a wonderful tourist attraction which is the Beruwala light
+                house. The island alone is a pleasure to the eye with its
+                greenery and the view of the Indian Ocean as well as its
+                peaceful ambience, the light house is simply a pleasing addition
+                to this green island known as Barbarian Island.
               </h4>
             </h3>
 
             <h3 className="card-title">
-              <b>Wadinagala </b>
+              {/* <b>Wadinagala </b> */}
               <h4>
-                Stands 2400 feet tall challenging the brave and the reckless. It
-                is famed to be a mountain, one considered as an alternative to
-                bear the foot marks of the Lord Buddha and its heritage in
-                ancient Buddhism is visible in many ruins scattered around the
-                mountain. Beautiful guard stone s, rocks pillars and moonstones
-                lie amidst the towering forest of ebony and stain wood, which is
-                been plundered by treasure hunters and illegal loggers.
-              </h4>
-            </h3>
-
-            <h3 className="card-title">
-              <b>Galoya National Park</b>
-              <h4>
-                Established in 1954 and serves as the main catchment area for
-                Senanayake Samudra, the largest reservoir in Sri Lanka.
-              </h4>
-            </h3>
-
-            <h3 className="card-title">
-              <b>Crocodile Rock </b>
-              <h4>
-                Lies at the river mouth where the Heda Oya falls to sea. Some
-                drip ledge caves, rock cut steps and ruins of ancient stupa can
-                be found on this rock.
-              </h4>
-            </h3>
-            <h3 className="card-title">
-              <b>Lahugala Magul Maha Viharaya </b>
-              <h4>
-                Ruins of this temple are one of the major tourist attractions of
-                the eastern province. And this temple is an archeologically
-                protected monument of the country.
-              </h4>
-            </h3>
-
-            <h3 className="card-title">
-              <b>Lahugala Kitulana National Park </b>
-              <h4>
-                One of the smallest national parks in Sri Lanka and it is an
-                important habitat for Sri Lankan elephants and endemic birds.
+                There are only four international lighthouse in Sri Lanka, one
+                being the Beruwala light house. All ships and boats going to the
+                Maldives have to pass the Beruwala lighthouse which is among the
+                most important place along the voyage.
               </h4>
             </h3>
           </p>
