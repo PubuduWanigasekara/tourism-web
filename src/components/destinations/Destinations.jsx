6764550d08b72051c8e29720b74fc8de6e8fb310
PubuduArosha <it18108514@my.sliit.lk>
2020-07-06 23:41:12 +0530

    Completed 91%

--- src/components/destinations/Destinations.jsx
+++ src/components/destinations/Destinations.jsx
@@ -41,7 +41,7 @@ function Destinations() {
       <div className="container-sm" style={containerStyle} id="mtn">
         <div>
           {/* Header */}
-          <div className="header">
+          <div className="header" style={{marginTop:"4%"}}>
             <h1>
               <b>Destinations</b>
             </h1>
